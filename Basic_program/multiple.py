6ebac4f0d1a415eb534bac0c5db695a64c76e5cb
Naveengupta2006 <naveengupta931844@gmail.com>
2026-01-06 23:37:39 +0530

    Sum of n natural number

--- Basic_program/multiple.py
+++
@@ -1,8 +0,0 @@
-def multiple_by_3_or_7(num):
-    if num % 3 == 0 or num % 7 == 0:
-        return "The number is multiple by 3 or 7"
-    else:
-        return "The number is not multiple by 3 or 7"
-    
-
-print(multiple_by_3_or_7(20))    
---
+++ Basic_program/sum_natural.py
@@ -0,0 +1,5 @@
+# Sum of first n natural number
+n = int(input('Enter a number: '))
+
+sum = n * (n + 1) // 2 # Formula for First n natural number.
+print('Sum of first ',n,"Natural number is: ",sum)
