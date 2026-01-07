3da51e07ad7376622f8704e34df0a4948537e381
Naveengupta2006 <naveengupta931844@gmail.com>
2026-01-07 23:10:14 +0530

    Count even odd

---
+++ count_even_odd.py
@@ -0,0 +1,16 @@
+def count_even_odd(numbers):
+    even = 0
+    odd = 0
+
+    for num in numbers:
+        if num % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+
+    print("Even number is : ")
+    print("odd number is :", odd)
+
+
+nums = [1,2,3,4,5,6,7,8]
+count_even_odd(nums)                
