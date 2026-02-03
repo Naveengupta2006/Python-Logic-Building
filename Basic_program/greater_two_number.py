aa5eec56068c202762a6d5a3c897fb73475b1139
Naveengupta2006 <naveengupta931844@gmail.com>
2026-02-03 20:24:04 +0530

    Find the largest of three numbers

---
+++ Basic_program/greater_two_number.py
@@ -0,0 +1,9 @@
+num1 = int(input('Enter a Number 1'))
+num2 = int(input('Enter a Number 2'))
+
+if num1 > num2:
+    print(f'{num1} is greater than {num2}')
+elif num2 > num1:
+    print(f'{num2} is greater than {num1}')
+else:
+    print('Both is equal ')    
