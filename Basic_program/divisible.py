a65ffc8a6252d0af7f0714582691c5c8cd153f83
Naveengupta2006 <naveengupta931844@gmail.com>
2026-02-03 20:34:55 +0530

    Check whether a number is divisible by 5 or 11.

---
+++ Basic_program/divisible.py
@@ -0,0 +1,9 @@
+num = int(input('Enter a number'))
+
+if num % 5 == 0:
+    print('Number divisible by 5:')
+elif num % 11 == 0:
+    print('Number divible by 11:')
+else:
+    print('Both not divible ')
+            
