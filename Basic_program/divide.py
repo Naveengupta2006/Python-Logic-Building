ab8da72c4633f09cf3a9b56ccee3bb6d0229f203
Naveengupta2006 <naveengupta931844@gmail.com>
2026-02-04 21:08:24 +0530

    Divide by 2 and 3 or both

---
+++ Basic_program/divide.py
@@ -0,0 +1,9 @@
+num = int(input('Enter a number'))
+
+if num % 2 == 0:
+    print('Divisible by 2')
+elif num % 3 == 0:
+    print('Divisible by 3')
+else:
+    print('Divisible by both')
+            
