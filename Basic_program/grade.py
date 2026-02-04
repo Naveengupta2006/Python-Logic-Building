5477486941bed491906cb8323fa65a21cc8ed5f9
Naveengupta2006 <naveengupta931844@gmail.com>
2026-02-04 21:02:50 +0530

    Grade Check to IF elif else

---
+++ Basic_program/grade.py
@@ -0,0 +1,14 @@
+grade = int(input('Enter a number: '))
+
+if grade >= 90:
+    print('Grade A')
+elif grade >= 75:
+    print('Grade B')
+elif grade >= 60:
+    print('Grade C')
+elif grade >= 40:
+    print('Grade D')
+else:
+    print('fail')
+    
+                    
