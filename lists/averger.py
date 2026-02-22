6c7499ed23aba4ec74200a63b6971587d821418f
Naveengupta2006 <naveengupta931844@gmail.com>
2026-02-22 19:51:15 +0530

    Find the average of elements.

---
+++ lists/averger.py
@@ -0,0 +1,10 @@
+# Find the average of elements.
+numbers = [20,40,50,20,60]
+total = 0
+
+# for loop
+for nums in numbers:
+    total += nums
+
+average = total / len(numbers)
+print(average)            
