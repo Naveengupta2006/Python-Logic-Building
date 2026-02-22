95d6779096b9bbc501ca9602a62a2b291253ec14
Naveengupta2006 <naveengupta931844@gmail.com>
2026-02-22 19:44:19 +0530

    lists using for loop in sum.

---
+++ Basic_program/slicing.py
@@ -0,0 +1,5 @@
+name = 'naveen'
+
+print(name[0:4])
+
+print(name[-4:-1])
---
+++ lists/sum.py
@@ -0,0 +1,9 @@
+# Find the sum of all elements.
+
+numbers = [10,20,30,40]
+
+total = 0
+for num in numbers:
+    total += num
+
+print(total)    
