3b458a761f36ed14de81b7d534d8d33173f1d970
Naveengupta2006 <naveengupta931844@gmail.com>
2026-03-19 19:58:49 +0530

    reverse integer

---
+++ loop/reverse_integer.py
@@ -0,0 +1,12 @@
+# Reverse a given integer number.
+
+num = int(input('enter a number: '))
+
+reverse = 0
+
+while num != 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print('Reverse is ', reverse)    
