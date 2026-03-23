b1fc7361fd7850a7f4065255945ef74f484b7ed1
Naveengupta2006 <naveengupta931844@gmail.com>
2026-03-23 21:17:19 +0530

    changes in code

--- string/lower_to_upper.py
+++ string/lower_to_upper.py
@@ -1,9 +1,9 @@
-s = "JSUbvduDUbudnchdnhdBCGD"
+s = "hfynMJDMDNndhfhfNFFJ"
 
 lowercase = [c for c in s if c.islower()]
 uppercase = [c for c in s if c.isupper()]
 
 result = "".join(lowercase + uppercase)
 
-print('Original', s)
-print('Result', result)
+print("original ",s)
+print("Result", result)
