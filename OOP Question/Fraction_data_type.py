a4b8abdc8a7ed14a3caa5af7e4f2d1950cf07603
Naveengupta2006 <naveengupta931844@gmail.com>
2026-04-12 20:51:48 +0530

    convert the fraction result to decimal

--- OOP Question/Fraction_data_type.py
+++ OOP Question/Fraction_data_type.py
@@ -31,12 +31,17 @@ class Fraction:
         new_den = self.den*other.num
 
         return '{}/{}'.format(new_num, new_den)
+
+    def convert_to_deciaml(self):
+        return self.num/self.den    
          
          
 fr1 = Fraction(3,4)
 fr2 = Fraction(1,2)
 
-print(fr1 + fr2)
+'''print(fr1 + fr2)
 print(fr1 - fr2)
 print(fr1 * fr2)
-print(fr1 / fr2)
+print(fr1 / fr2)'''
+
+print(fr1.convert_to_deciaml())
