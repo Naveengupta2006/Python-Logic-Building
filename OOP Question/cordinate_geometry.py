937103837134f6b774a888db87055d43a85978ea
Naveengupta2006 <naveengupta931844@gmail.com>
2026-04-19 15:14:05 +0530

    inside the create line object

--- OOP Question/cordinate_geometry.py
+++ OOP Question/cordinate_geometry.py
@@ -28,15 +28,34 @@ class point:
         return self.enclidean_distance(point(0,0))
     
     # A user can check if a point lies on a given line
+    # can define the user (Ax + Bx + c)
+
+
+class line:
     
-        
+    def __init__(self,A,B,C):
+        self.A = A
+        self.B = B
+        self.C = C    
+
+    def __str__(self):
+        return '{}x + {}y + {} = 0'.format(self.A, self.B, self.C)
+
+    def point_on_line(line,point):
+        if line.A*point.x_cod + line.B*point.y_cod + line.C == 0:
+            return "lies on the line"
+        else:
+            return "does not lie on the line"
+
     
     
 
 
 
-p1 = point(0,0)
-p2 = point(1,1)
+l1 = line(1,1,-2)
+p1 = point(1,1)
 
-# print(p1.enclidean_distance(p2))
-print(p1.distance_from_origin())
+print(l1)
+print(p1)
+
+print(l1.point_on_line(p1))
