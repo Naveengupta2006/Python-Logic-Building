3da1e4124f8fd7a6315270fdb157fe2aab63e23f
Naveengupta2006 <naveengupta931844@gmail.com>
2026-05-02 16:42:46 +0530

    inhertance pratice.

---
+++ OOP Question/get_inherited_4.py
@@ -0,0 +1,16 @@
+class parent:
+
+    def __init__(self, num):
+        self.__num = num
+
+    def get_num(self):
+        return self.__num
+
+class child(parent):
+
+    def show(self):
+        print('this is in child class')
+
+son = child(100)
+print(son.get_num())
+son.show()                
