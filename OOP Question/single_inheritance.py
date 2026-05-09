22f6081a00fe5c8d27d2a09f23b38cffcec5d1ce
Naveengupta2006 <naveengupta931844@gmail.com>
2026-05-10 00:00:44 +0530

    this is single inhertance

---
+++ OOP Question/single_inheritance.py
@@ -0,0 +1,14 @@
+class phone:
+    def __init__(self, price, brand, camera):
+        print("Inside phone construtor")
+        self.__price = price
+        self.brand = brand
+        self.camera = camera
+
+    def buy(self):
+        print("Buying a phone")
+
+class smartphone(phone):
+    pass
+
+smartphone(1000,"apple","13px").buy()
