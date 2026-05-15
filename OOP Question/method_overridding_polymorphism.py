66bfc6616a3414f72b447d37af5e1ded91c508fd
Naveengupta2006 <naveengupta931844@gmail.com>
2026-05-15 16:18:46 +0530

    method_overloading

---
+++ OOP Question/method_overridding_polymorphism.py
@@ -0,0 +1,14 @@
+# method overloading
+
+class shape:
+
+    def area(self, a,b=0):
+        if b == 0:
+            return 3.14*a*a # area of circle
+        else:
+            return a*b 
+
+s = shape()
+
+print(s.area(2))
+print(s.area(3,4))
