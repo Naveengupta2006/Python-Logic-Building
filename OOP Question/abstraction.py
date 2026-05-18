1d8554f5b6cfa2cc54a2f2a3126458acbca941aa
Naveengupta2006 <naveengupta931844@gmail.com>
2026-05-18 14:06:02 +0530

    abstraction code

---
+++ OOP Question/abstraction.py
@@ -0,0 +1,25 @@
+# abstraction
+
+# abstract class
+
+from abc import ABC,abstractmethod
+class bankapp(ABC):
+
+    def database(self):
+        print('connected to database')
+
+    @abstractmethod
+    def security(self):
+        pass
+
+class mobileapp(bankapp):
+
+    def mobile_login(self):
+        print('login into mobile')
+
+    def security(self):
+        print('mobile security')
+
+mob = mobileapp()
+
+print(mob.security())
