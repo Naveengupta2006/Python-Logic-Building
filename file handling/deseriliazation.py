6a2bdb1cd161c3d284807b21b68d606cc148ed5d
Naveengupta2006 <naveengupta931844@gmail.com>
2026-07-08 12:33:42 +0530

    feat: add JSON deserialization script and update demo data to a numeric list

--- file handling/deseriliazation.py
+++ file handling/deseriliazation.py
@@ -4,3 +4,11 @@ with open('demo.json','r') as f:
     d = json.load(f)
     print(d)
     print(type(d))
+
+# Serialize and deserialize tuple
+import json
+
+t = (1,2,3,4,5)
+
+with open('demo.json','w') as f:
+    json.dump(t,f)
