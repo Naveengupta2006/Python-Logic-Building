3f652db85b51b53cea0f8c67cf2181f9d0de6360
Naveengupta2006 <naveengupta931844@gmail.com>
2026-07-08 13:01:11 +0530

    feat: add script for JSON deserialization and update demo data structure

--- file handling/deseriliazation.py
+++ file handling/deseriliazation.py
@@ -12,3 +12,14 @@ t = (1,2,3,4,5)
 
 with open('demo.json','w') as f:
     json.dump(t,f)
+
+# serialize and deserialize a nested dict
+
+d = {
+    'student':'naveen',
+    'marks':[12,3,4,56,56]
+
+}
+
+with open('demo.json','w') as f:
+    json.dump(d,f)
