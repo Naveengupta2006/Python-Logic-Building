1c22ff122f9af1a9623eb0de8f9929377a159762
Naveengupta2006 <naveengupta931844@gmail.com>
2026-07-09 23:58:58 +0530

    feat: add example scripts demonstrating various Python exception types and handling techniques

---
+++ exception_handling.py/exception_2.py
@@ -0,0 +1,3 @@
+# why is it important to handle exceptions
+# how to handle exceptions
+# -> try catch block
---
+++ exception_handling.py/syntax_error.py
@@ -0,0 +1,27 @@
+# example of syntex error
+# print 'hello world'
+
+# indexerror
+# the indexerror is thrown when trying to access an item at an invalid index.
+'''l = [1,2,3]
+l[100]'''
+
+# Modulenotfounderror
+# the modulenotfound error is thrown when a module could not be found.
+
+''''import mathi
+math.floor(5.3)'''
+
+# keyerror
+'''d = {'name':'Aman','age':23}
+d['salary']'''
+
+# type error
+#1 + 'a'
+
+# name error
+#print(k)
+
+# atribute errir
+l = [1,2,3]
+l.upper()
