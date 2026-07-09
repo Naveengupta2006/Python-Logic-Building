bfc8558d7d27baf1a168bb17711acf3d1092eca1
Naveengupta2006 <naveengupta931844@gmail.com>
2026-07-09 14:36:45 +0530

    feat: implement file reading utility and object pickling demonstration scripts

--- file handling/pickling.py
+++ file handling/pickling.py
@@ -1,2 +1,22 @@
 class Person:
-    
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print('Hi my name is ',self.name,'and i am ',self.age,'years old')
+
+p = Person('naveen',20)
+
+# pickle dump
+import pickle
+
+with open('person.pkl','wb') as f:
+    pickle.dump(p,f)
+
+import pickle
+
+with open('person.pkl','rb') as f:
+    pickle.load(f)
+
+
---
+++ file handling/question 1.py
@@ -0,0 +1,18 @@
+'''Q-1: Write a function get_final_line(filename), which takes filename as input and return final line of the file.
+Note: You can choose any file of your choice.'''
+
+def get_final_line(filename):
+    with open(filename, 'r') as f:
+        return f.readlines()[-1]
+
+# Create the file with some content
+with open('filename.txt', 'w') as f:
+    f.write("data science\n")
+    f.write('python\n')
+    f.write('deep learning\n')
+    f.write('java\n')
+
+# Call the function and print the returned final line
+file = get_final_line('filename.txt')
+print(file)
+
