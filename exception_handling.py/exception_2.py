c15eaa2fd343df42f5828a463eff10784afd811e
Naveengupta2006 <naveengupta931844@gmail.com>
2026-07-12 22:51:49 +0530

    feat: add exception handling demonstration script and sample text file

--- exception_handling.py/exception_2.py
+++ exception_handling.py/exception_2.py
@@ -1,3 +1,25 @@
 # why is it important to handle exceptions
 # how to handle exceptions
-# -> try catch block
+# -> try except block
+
+# let create a file
+
+with open('sample.txt','w') as f:
+    f.write('hello')
+
+# try except demo
+try:
+    with open('sample2.txt','r') as f:
+        print(f.read())
+except FileNotFoundError:
+    print('File not found')
+
+
+# catching specific exception
+try:
+    f = open('sample2.txt','r') 
+    print(f.read())
+    print(m)
+except FileNotFoundError:
+    print('some error occured')
+
